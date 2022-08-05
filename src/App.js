aaaec73a4a5434dcad7d4dbac4f0cd4fafaf6ea9
이정배 <jblee@bankbe.net>
2022-08-05 17:09:17 +0900

    Add: 폰트 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import MainRoutes from "./routes";
+import GlobalStyles from "themes/GlobalStyles";
 
 function App() {
   return (
     <div className="App">
+        <GlobalStyles />
         <MainRoutes />
     </div>
   );
