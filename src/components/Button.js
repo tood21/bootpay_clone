45b15e4c03e4356a6def6e6805f02292e6dd794c
이정배 <jblee@bankbe.net>
2022-08-08 17:28:15 +0900

    Update: 버튼 hover 스타일링

--- src/components/Button.js
+++ src/components/Button.js
@@ -17,4 +17,11 @@ const StyledButton = styled.a`
   font-size: 1.3rem;
   padding: 16px 30px;
   background: #4529e5;
+  border-radius: 3px;
+  &:hover {
+    margin-top: -2px;
+    margin-bottom: 2px;
+    transition: all 0.3s;
+    box-shadow: 0 5px 12px rgb(0 0 0 / 20%);
+  }
 `
