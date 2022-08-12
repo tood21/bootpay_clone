e4aa44ee4ab3124f059c774effdea249aa278347
이정배 <jblee@bankbe.net>
2022-08-12 13:58:46 +0900

    Add: 이벤트추가: 페이지 이동시 스크롤 최상단으로

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import MainRoutes from "./routes";
 import GlobalStyles from "themes/GlobalStyles";
 import "./App.css"
+
 import styled from "styled-components";
+import ScrollTop from "./util/ScrollTop";
 
 function App() {
   return (
     <Container className="App">
+        <ScrollTop />
         <GlobalStyles />
         <MainRoutes />
     </Container>
