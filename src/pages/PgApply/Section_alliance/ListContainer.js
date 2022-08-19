aa3221f881463ab3832ca82ef55cb1e454ba22e6
이정배 <jblee@bankbe.net>
2022-08-19 14:03:08 +0900

    Add: PG가입하기 페이지 section_alliance UI 제작

--- src/pages/PgApply/Section_alliance/ListContainer.js
+++ src/pages/PgApply/Section_alliance/ListContainer.js
@@ -3,7 +3,7 @@ import React from 'react'
 import listData from "./listData";
 import styled from "styled-components";
 
-
+import question from "assets/images/PgApply/Section_alliance/question.svg"
 
 
 
@@ -14,21 +14,36 @@ const ListContainer = () => {
                 {
                     listData.map((list) => (
                         <CardContainer key={list.id}>
+                            {
+                                list.title &&
+                                <Card>
+                                    <div><img  src={list.url}/></div>
+                                    <CommissionList>
+                                        {
+                                            list.fee.map((item) => (
+                                                <li key={item.kind}>
+                                                    <p>{item.kind}</p>
+                                                    <p>{item.value}</p>
+                                                </li>
+                                            ))
+                                        }
+                                    </CommissionList>
+                                    <SubmitButton>가입 신청하기</SubmitButton>
+                                </Card>
+                            }
 
-                            <Card>
-                                <div><img  src={list.url}/></div>
-                                <CommissionList>
-                                    {
-                                        list.fee.map((item) => (
-                                            <li key={item.kind}>
-                                                <p>{item.kind}</p>
-                                                <p>{item.value}</p>
-                                            </li>
-                                        ))
-                                    }
-                                </CommissionList>
-                                <SubmitButton>가입 신청하기</SubmitButton>
-                            </Card>
+                            {
+                                !list.title &&
+                                <Card>
+                                    <CardTitle>
+                                        <p>어떤 PG사를 이용해야 할지 모르겠다면</p>
+                                        <p>나에게 <span>알맞은 PG사</span>를<br/>추천받으세요</p>
+                                    </CardTitle>
+                                    <CardBody><img src={question} alt="물음표" /></CardBody>
+                                    <SubmitButton>가입 신청하기</SubmitButton>
+                                </Card>
+                            }
+
                         </CardContainer>
                     ))
                 }
@@ -54,12 +69,15 @@ const CardContainer = styled.div`
 const Card = styled.li`
   background-color: #fff;
   box-shadow: 1px 1px 10px rgb(170 170 170 / 20%);
-  padding: 20px;
+  padding: 16px 20px;
+  height: 338px;
   > div {
-    text-align: center;
-    margin-bottom: 10px;
-    img {
-      width: 138px;
+    &:nth-child(1) {
+      text-align: center;
+      margin-bottom: 10px;
+      img {
+        width: 138px;
+      }
     }
   }
 `
@@ -98,4 +116,36 @@ const SubmitButton = styled.button`
   padding: 15px 0;
   font-size: 0.8125rem;
   line-height: 1.5;
+`
+
+const CardTitle = styled.div`
+  p {
+    color: #2f374d;
+    text-align: left;
+    &:nth-child(1) {
+      font-size: 0.75rem;
+      line-height: 1.6;
+      padding-top: 15px;
+    }
+    
+    &:nth-child(2) {
+      font-size: 1.375rem;
+      line-height: 1.3;
+      font-weight: 700;
+      padding-bottom: 10px;
+      span {
+        color: #71d89c;
+      }
+    }
+  }
+`
+const CardBody = styled.div`
+  text-align: center;
+    img {
+      width: 100px;
+      height: 95px;
+      margin-bottom: 40px;
+    }
+
+    
 `
